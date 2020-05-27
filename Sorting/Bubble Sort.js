8a772cefbb2b74576f39ae897eb9a184be239b35
thenatural86 <philip.smalls86@gmail.com>
2020-05-27 18:11:23 -0400

    pseudo code

--- Sorting/Bubble Sort.js
+++ Sorting/Bubble Sort.js
@@ -6,3 +6,65 @@
 // very common task in programming
 // different sorting algos have pros and cons
 // Basic Sorting algos are: bubble, selection, insertion
+
+// JavaScript built in sort
+// Sort an array of string alphabetically
+// Sort an array of numbers doesn't have discernable pattern
+// According to MDN, default sort order is "according to string Unicode code points."
+// Converts each element into a string and then the value is taken from that and then sorted. Leads to unexpected behavior
+
+// Tell JS how to sort
+// built in sort method accepts an optional comparator function
+// Used to tell JS how we want to sort
+// Looks at a pair of elements (a, b) and determines their sort order based on the return val
+// if negative (-) number is returned, a comes before b
+// if positive (+) number is returned, b comes before a
+// if 0 is returned, a and b are the same
+
+function compare(a, b) {
+  return a - b
+}
+function numCompare(num1, num2) {
+  return num1 - num2
+}
+function strCompare(str1, str2) {
+  return str1 - str2
+}
+
+// Bubble Sort Overview
+// A sorting algo where the largest values bubble to the top
+// As we loop through each item, we compare it to the next item in the array
+// We check if it is larger than the one we are comparing it to, if it is larger, we swap
+
+let exampleArray = [5, 3, 4, 1, 2]
+
+// compare 5 and 3
+// swap, [3,5,4,1,2]
+// compare 5 and 4
+// swap [3,4,5,1,2]
+// compare 5 and 1
+// swap [3,4,1,5,2]
+// compare 5 and 2
+// swap [3,4,1,2,5]
+// one iteration, need to repeat process
+
+// So how do we swap the elements?
+// Many sorting algos have some sort of swapping functionality
+
+// es5
+function swap(arr, idx1, idx2) {
+  let temp = arr[idx1]
+  arr[idx1] = arr[idx2]
+  arr[idx2] = temp
+}
+
+// es6
+const swap = (arr, idx1, idx2) => {
+  ;[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+}
+
+// Pseudo Bubble Sort
+// function that takes in an array of number
+// start looping with variable i from the end of the array towards the beginning
+// start an inner loop with variable j from the beginning until i -1
+// if arr[j] is greater than arr[j + 1], swap those two values
