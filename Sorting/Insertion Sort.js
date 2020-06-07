0dc1ec12dcac187a929b5511b6723e98d99198aa
thenatural86 <philip.smalls86@gmail.com>
2020-06-07 13:57:06 -0400

    insertion sort

--- Sorting/Insertion Sort.js
+++ Sorting/Insertion Sort.js
@@ -6,3 +6,16 @@ let array = [5, 3, 4, 1, 2]
 // Start by picking the second element in  the array because the "sorted portion" will be considered the 1st element
 // Compare to the 2nd element to the one before it and swap if necessary
 // Continue to the next element and if it is in the incorrect order, iterate through the sorted portion, the left side, to place the element in the correct position.
+
+function insertion(arr) {
+  for (i = 1; i < arr.length; i++) {
+    let currentVal = arr[i]
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j++) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentVal
+  }
+  return arr
+}
+
+// Big O - O(n^2)
