26f4d75256fdfbfd483b80ef6021b6260cab05b9
thenatural86 <philip.smalls86@gmail.com>
2020-07-01 12:26:25 -0400

    DLL

--- Data Structures/Doubly Linked List.js
+++ Data Structures/Doubly Linked List.js
@@ -7,3 +7,27 @@
 // DLL
 // Almost identical to SLL
 // Except DLL has a pointer to the next node AND the previous node
+
+// Comparisons with SLL
+// more memory === more flexibility
+
+// Node
+// val
+// next
+// prev
+
+// DoublyLinkedList
+// head
+// tail
+// length
+
+// push()
+// Adding a node to the end of a DLL
+
+// create a new node with the value passed to the function
+// if the head property is null set the head and tail to be the newly created node
+// if not, set the next property on the tail to be that node
+// set the previous property in the newly created node to be the tail
+// set the tail to be the newly created node
+// increment the length
+// return the DLL
