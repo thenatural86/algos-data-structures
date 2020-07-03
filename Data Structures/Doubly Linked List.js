7de37abb018c1998ccb39b29d531163cd2cc4c6e
thenatural86 <philip.smalls86@gmail.com>
2020-07-03 18:25:31 -0400

    remove

--- Data Structures/Doubly Linked List.js
+++ Data Structures/Doubly Linked List.js
@@ -217,4 +217,22 @@ class DoublyLinkedList {
     this.length++
     return true
   }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined
+    if (index === 0) return this.shift()
+    if (index === this.length - 1) return this.pop()
+
+    let removedNode = this.get(index)
+    let beforeNode = removedNode.prev
+    let afterNode = removedNode.next
+
+    beforeNode.next = afterNode
+    afterNode.prev = beforeNode
+
+    removedNode.next = null
+    removedNode.prev = null
+
+    this.length--
+    return removedNode
+  }
 }
