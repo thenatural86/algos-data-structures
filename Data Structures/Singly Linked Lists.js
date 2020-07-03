49f728a0c6ad1f1c057587e440eebd7f48966bdf
thenatural86 <philip.smalls86@gmail.com>
2020-07-03 17:19:50 -0400

    SLL

--- Data Structures/Singly Linked Lists.js
+++ Data Structures/Singly Linked Lists.js
@@ -30,13 +30,6 @@
 //  stores a piece of data - val
 //  stores a reference to the next node - next
 
-class Node {
-  constructor(val) {
-    this.val = val
-    this.next = null
-  }
-}
-
 // let first = new Node("Hi")
 // first.next = new Node("there")
 // first.next.next = new Node("how")
@@ -45,6 +38,136 @@ class Node {
 
 // do not initialize with any args, rather define with
 // a pointer to head, pointer to tail and a length
+//  work on adding many methods to SinglyLinkedList class
+
+// .push()
+// takes a piece of data create a new Node and assign it to
+// the head and tail of the list. Assigned to both when list
+// is empty (80).
+// Next we take the head and have its next
+// property point to the next node and set tail to be (81)
+// then take current tail and have it point to (85), then
+// update that to be the tail.
+
+// push()
+// adding a new Node to the end of the Linked List
+
+// function should accept a value
+// create a new node using the value passed into the
+// function
+// If there is no head property on the list, set the head
+// and the tail to be the newly created node
+// Otherwise set the next property on the tail to be the
+// new node and set the tail property on the list
+// to be the newly created node
+// Increment the length by one
+
+// pop()
+// popping is removing a node from the end of Linked List
+
+// to remove tail, we must assign a new tail
+// by starting at beginning and going through list until
+// end and making the last node the new tail
+// first thing we need to be able to do is traverse
+// the list
+
+// Pseudo
+// if there are no nodes in the list, return undefined
+// loop through the list until you reach the tail
+// set the next property of the 2nd to last node to be
+// null
+// set the tail to be the 2nd to last node
+// decrement the length of the list by 1
+// return the value of the node removed
+
+// shift()
+// shifting is removing a new node from the beginning of the Linked List
+
+// if there ar no nodes, return undefined
+// store the current head property in a variable
+// set the head property to be the current head's next property
+// decrement the length by 1
+// return the value of the node removed
+
+// unshift()
+// adding a new node the beginning of the LL
+
+// accepts a value
+// create a new node using the value passed to the function
+// if there is no head property on the list, set the head and tail
+// to be the newly created node
+// otherwise set the newly created nodes next property to be the
+// current head property on the list
+// set the head property on the list to be the newly created node
+// increment the length of list by 1
+// return LL
+
+// get()
+// retrieve a node by it's position in the linked list
+
+// should accept an index
+// if the index is less than zero or greater than or equal to the
+// length of the of the list, return null
+// loop through the list until you reach the index and return the
+// node at that specific index
+
+// set()
+// changing the value of a node based on it's position in the LL
+
+// accept an index and value
+// use get method to find the specific node
+// if the node is not found return false
+// if node is found, set the value of that node to be the value
+// passed to the function and return true
+
+// insert()
+// adding a node to the LL at a specific position
+
+// if the index is less than zero or greater than the length,
+// return false
+// if the index is the same as the length, push a new node to
+// the end of the list
+// if the index is 0, unshift a new node to the start of the list
+// otherwise, using the get method, access the node at
+// the index -1
+// set the next property on that node to be the new node
+// set the next property on the new node to be the previous next
+// increment the length
+// return true
+
+// remove()
+// removing a node from the LL at a specific position
+
+// if the index is less than zero or greater than the length
+// return undefined
+// if the index if the same as the length - 1, pop
+// if the index is zero, shift
+// otherwise, using the get method, access the node at
+// the index - 1
+// set the next property on that node to be the next of the
+// next node
+// decrement the length
+// return the value of the removed node
+
+// reverse()
+// reverse the ll in place
+
+// swap the head and the tail
+// create next variable
+// create previous variable
+// create node variable and initialize to the head property
+// loop through list
+// set next to be the next property on whatever node is
+// set the next property on the node to be whatever previous is
+// set previous to be the value of the node variable
+// set the node variable to be the value of the next variable
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
 class SinglyLinkedList {
   constructor() {
     this.head = null
@@ -52,29 +175,6 @@ class SinglyLinkedList {
     this.length = 0
   }
 
-  //  work on adding many methods to SinglyLinkedList class
-  // .push()
-  // takes a piece of data create a new Node and assign it to
-  // the head and tail of the list. Assigned to both when list
-  // is empty (80).
-  // Next we take the head and have its next
-  // property point to the next node and set tail to be (81)
-  // then take current tail and have it point to (85), then
-  // update that to be the tail.
-
-  // Push
-  // adding a new Node to the end of the Linked List
-
-  // Pseudo
-  // function should accept a value
-  // create a new node using the value passed into the
-  // function
-  // If there is no head property on the list, set the head
-  // and the tail to be the newly created node
-  // Otherwise set the next property on the tail to be the
-  // new node and set the tail property on the list
-  // to be the newly created node
-  // Increment the length by one
   push(val) {
     let newNode = new Node(val)
     if (!this.head) {
@@ -87,25 +187,6 @@ class SinglyLinkedList {
     this.length++
     return this
   }
-
-  // Pop
-
-  // popping is removing a node from the end of Linked
-  // List
-  // to remove tail, we must assign a new tail
-  // by starting at beginning and going through list until
-  // end and making the last node the new tail
-  // first thing we need to be able to do is traverse
-  // the list
-
-  // Pseudo
-  // if there are no nodes in the list, return undefined
-  // loop through the list until you reach the tail
-  // set the next property of the 2nd to last node to be
-  // null
-  // set the tail to be the 2nd to last node
-  // decrement the length of the list by 1
-  // return the value of the node removed
 
   pop() {
     if (!this.head) return undefined
@@ -133,18 +214,6 @@ class SinglyLinkedList {
     return current
   }
 
-  // Shift
-
-  // shifting is removing a new node from the beginning of the Linked
-  // List
-
-  // Pseudo
-
-  // if there ar no nodes, return undefined
-  // store the current head property in a variable
-  // set the head property to be the current head's next property
-  // decrement the length by 1
-  // return the value of the node removed
   shift() {
     if (!this.head) return undefined
     let current = this.head
@@ -152,20 +221,6 @@ class SinglyLinkedList {
     this.length--
     return current
   }
-
-  // Unshift
-  // adding a new node the beginning of the LL
-
-  // Pseudo
-  // accepts a value
-  // create a new node using the value passed to the function
-  // if there is no head property on the list, set the head and tail
-  // to be the newly created node
-  // otherwise set the newly created nodes next property to be the
-  // current head property on the list
-  // set the head property on the list to be the newly created node
-  // increment the length of list by 1
-  // return LL
 
   unshift(val) {
     let newNode = new Node(val)
@@ -180,16 +235,6 @@ class SinglyLinkedList {
     }
   }
 
-  // Get
-  // retrieve a node by it's position in the linked list
-
-  // Pseudo
-  // should accept an index
-  // if the index is less than zero or greater than or equal to the
-  // length of the of the list, return null
-  // loop through the list until you reach the index and return the
-  // node at that specific index
-
   get(idx) {
     if (idx < 0 || idx >= this.length) return null
     let counter = 0
@@ -201,15 +246,6 @@ class SinglyLinkedList {
     return current
   }
 
-  // Set
-  // changing the value of a node based on it's position in the LL
-
-  // accept an index and value
-  // use get method to find the specific node
-  // if the node is not found return false
-  // if node is found, set the value of that node to be the value
-  // passed to the function and return true
-
   set(idx, val) {
     foundNode = this.get(idx)
     if (foundNode) {
@@ -218,22 +254,6 @@ class SinglyLinkedList {
     }
     return false
   }
-
-  // Insert
-  // adding a node to the LL at a specific position
-
-  // Pseudo
-  // if the index is less than zero or greater than the length,
-  // return false
-  // if the index is the same as the length, push a new node to
-  // the end of the list
-  // if the index is 0, unshift a new node to the start of the list
-  // otherwise, using the get method, access the node at
-  // the index -1
-  // set the next property on that node to be the new node
-  // set the next property on the new node to be the previous next
-  // increment the length
-  // return true
 
   insert(idx, val) {
     if (idx < 0 || idx > this.length) return false
@@ -249,21 +269,6 @@ class SinglyLinkedList {
     return true
   }
 
-  // Remove
-  // removing a node from the LL at a specific position
-
-  // Pseudo
-  // if the index is less than zero or greater than the length
-  // return undefined
-  // if the index if the same as the length - 1, pop
-  // if the index is zero, shift
-  // otherwise, using the get method, access the node at
-  // the index - 1
-  // set the next property on that node to be the next of the
-  // next node
-  // decrement the length
-  // return the value of the removed node
-
   remove(idx) {
     if (idx < 0 || idx >= this.length) return undefined
     if (idx === 0) return this.shift()
@@ -276,28 +281,12 @@ class SinglyLinkedList {
     return removed
   }
 
-  // Reverse
-  // reverse the ll in place
-
-  // Pseudo
-  // swap the head and the tail
-  // create next variable
-  // create previous variable
-  // create node variable and initialize to the head property
-  // loop through list
-  // set next to be the next property on whatever node is
-  // set the next property on the node to be whatever previous is
-  // set previous to be the value of the node variable
-  // set the node variable to be the value of the next variable
-
   reverse() {
     let node = this.head
     this.head = this.tail
     this.tail = node
-
     let next
     let prev = null
-
     for (let i = 0; i < this.length; i++) {
       next = node.next
       node.next = prev
@@ -331,7 +320,5 @@ list.push("!")
 // Recap
 
 // Singly Linked List are an excellent alternative to arrays when insertion and deletion at the beginning are frequently required
-
 // Arrays contain a built in index where as linked list do not
-
 // The idea of a list data structure that consists of nodes is the foundation for other data structures like stacks and queues
