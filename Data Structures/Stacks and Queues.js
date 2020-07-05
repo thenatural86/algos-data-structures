c12efd1583b4fb805171da570ab946afc5818d44
thenatural86 <philip.smalls86@gmail.com>
2020-07-05 16:39:23 -0400

    stacks

---
+++ Data Structures/Stacks and Queues.js
@@ -0,0 +1,12 @@
+// Objectives
+// define what a stack is
+// understand use cases for a stack
+// implement operations in a stack
+
+// What is a stack?
+// an abstract data structure that abides by the LIFO rules
+// Last In First Out
+// the last element added to the stack will be the first element removed from the stack
+
+// Where stacks are used
+// managing function invocations
