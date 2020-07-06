974fc348f799bfecfcac339ed1923704a42d9e73
thenatural86 <philip.smalls86@gmail.com>
2020-07-06 15:12:53 -0400

    queues

--- Data Structures/Queues.js
+++ Data Structures/Queues.js
@@ -1,1 +1,24 @@
 // Objectives
+// define what a queue is
+// understand use cases for a queue
+// implement operations on a queue data structure
+
+// What is a Queue
+// A queue is a FIFO - first in first out - data structure
+// Can built a queue using an array but not ideal because needs to be re-indexed
+// Can build your own
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+}
