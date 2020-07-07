a13b03ced7fccd67285f9cd2c48196ce71fab4f4
thenatural86 <philip.smalls86@gmail.com>
2020-07-07 13:58:24 -0400

    insert

--- Data Structures/Binary Search Trees.js
+++ Data Structures/Binary Search Trees.js
@@ -61,10 +61,36 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
+  insert(value) {
+    let newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+      return this
+    } else {
+      let current = this.root
+      while (true) {
+        if (value === current.value) return undefined
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode
+            return this
+          } else {
+            current = current.left
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode
+            return this
+          } else {
+            current = current.right
+          }
+        }
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree()
-tree.root = new Node(10)
-tree.root.right = new Node(15)
-tree.root.left = new Node(7)
-tree.root.left.right = new Node(9)
+tree.insert(10)
+tree.insert(5)
+tree.insert(2)
