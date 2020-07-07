6d9cd9425b5db6cd67d4fe49fa47a536f1398e5d
thenatural86 <philip.smalls86@gmail.com>
2020-07-07 12:12:53 -0400

    BST

--- Data Structures/Binary Search Trees.js
+++ Data Structures/Binary Search Trees.js
@@ -2,6 +2,21 @@
 // Define what a tree is
 // Compare and contrast trees and list
 // Explain difference between trees, binary trees, binary search trees
+// Implement operations on BST
+
+// What is a Tree?
+// A data structure that consist of nodes in a parent/child relationship
+
+// List - linear
+// Tress - nonlinear
+
+// Tree terminology
+// Root - The top node in a tree
+// Child - A node directly connected to another node when moving away from the root
+// Parent - The converse notion of a child
+// Siblings - A group of nodes with the same parent
+// Leaf - A node with no children
+// Edge - The connection between one node and another
 
 // needs to have root property that is root by default and that's it
 
