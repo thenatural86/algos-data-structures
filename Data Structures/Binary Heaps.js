20e9234db61b232dd118555007dc641a1df3b2fc
thenatural86 <philip.smalls86@gmail.com>
2020-07-14 12:03:08 -0400

    heaps

--- Data Structures/Binary Heaps.js
+++ Data Structures/Binary Heaps.js
@@ -3,3 +3,18 @@
 // Compare and contrast min and max heaps
 // Implement basic methods on heaps
 // Understand where heaps are used in the real world and what other data structures can be constructed from heaps
+
+// What is A Binary Heap
+// Very similar to a binary search tree but with some different rules
+// In a Max Binary Heap, parent nodes are always larger than child nodes
+// In a Min Binary Heap, parent nodes are always smaller than child nodes
+
+// Max Binary Heap
+// Each parent has at most two child nodes
+// The value of each parent node is always greater than its child nodes
+// The parent is greater than the child but there are no guarantees with sibling nodes
+// A Binary Heap is as compact as possible. All the children of each node are as full as they can be and left children are filled out first
+
+// Why do we need to know this?
+// Binary Heaps are use to implement Priority Queues, which are very commonly used data structures
+// They are also used quite a bit with graph traversal algorithms
