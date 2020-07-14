774c5fdc56cfd8188237cd3940bca4b08e15698e
thenatural86 <philip.smalls86@gmail.com>
2020-07-14 16:23:28 -0400

    heaps

--- Data Structures/Binary Heaps.js
+++ Data Structures/Binary Heaps.js
@@ -18,3 +18,23 @@
 // Why do we need to know this?
 // Binary Heaps are use to implement Priority Queues, which are very commonly used data structures
 // They are also used quite a bit with graph traversal algorithms
+
+// Storing Heaps
+// We can store a heap in an array.
+// To locate the children of nodes in an array we can use this formula:
+// For any index of an array n
+// The left child is stored at 2n + 1
+// The right child is stored at 2n + 2
+
+// What if we have a child node and want to find it's parent
+// For any child at index n
+// It's parent is at (n-1) / 2 and floor it
+
+// insert()
+// push the value into the values property on the heap
+// Bubble up:
+// create a variable called index which is the length of the values property - 1
+// create a variable called parentIndex, which is the floor of (index-1)/2
+// keep looping as long as the values element at the parentIndex is less than the values element at the child index
+// swap the value of the values element at the parentIndex with the value of the element property at the child index
+// set the index to be the parentIndex and start over
