6667e8eeafccc027f4935bf983c087b3f0f02fe4
thenatural86 <philip.smalls86@gmail.com>
2020-07-14 11:34:36 -0400

    heaps

---
+++ Data Structures/Binary Heaps.js
@@ -0,0 +1,5 @@
+// Objectives
+// Define what a Binary Heap is
+// Compare and contrast min and max heaps
+// Implement basic methods on heaps
+// Understand where heaps are used in the real world and what other data structures can be constructed from heaps
