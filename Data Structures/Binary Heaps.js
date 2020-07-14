99b9f407ace54a913c3f4caf18fa651c724702da
thenatural86 <philip.smalls86@gmail.com>
2020-07-14 16:24:08 -0400

    insert

--- Data Structures/Binary Heaps.js
+++ Data Structures/Binary Heaps.js
@@ -38,3 +38,28 @@
 // keep looping as long as the values element at the parentIndex is less than the values element at the child index
 // swap the value of the values element at the parentIndex with the value of the element property at the child index
 // set the index to be the parentIndex and start over
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = []
+  }
+  insert(element) {
+    this.values.push(element)
+    this.bubbleUp()
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIdx]
+      if (element <= parent) break
+      this.values[parentIdx] = element
+      this.values[idx] = parent
+      idx = parentIdx
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
