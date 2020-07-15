2ea6d10aed32b92d14ddcbce3ce9c4ab24f973fb
thenatural86 <philip.smalls86@gmail.com>
2020-07-15 12:41:24 -0400

    extractMax

--- Data Structures/Binary Heaps.js
+++ Data Structures/Binary Heaps.js
@@ -39,11 +39,21 @@
 // swap the value of the values element at the parentIndex with the value of the element property at the child index
 // set the index to be the parentIndex and start over
 
-// remove()
+// extractMax()
 // remove the root
 // replace with the most recently added
 // adjust (sink down) - the procedure for deleting the root from the heap (effectively extracting the maximum element in a max heap or the minimum element in a min-heap) and restoring the properties is called down-heap or sink-down/bubbledown
 
+// swap the first value in the values property with the last one
+// pop from the values property, so you can return the value at the end
+// have the new root bubble down to the correct spot
+// your parent index starts at 0 (the root)
+// find the index of the left child: 2 * index + 1 (make sure it is not out of bounds)
+// find the index of the right child: 2 * index + 2 (make sure it is nor out of bounds)
+// if the left of right child is greater than the element ... swap. if both left and right children are larger, swap with the largest child
+// the child index we swapped to now becomes the new parent index
+// keep looping and swapping until neither child is larger than the element
+// return the old root
 class MaxBinaryHeap {
   constructor() {
     this.values = []
